979b7393217eeaa6d327551cf67e7ae76b11ae93
Eric Hayen <ehayen@gmail.com>
2025-09-11 16:21:27 -0700

    init commit, setup of classes

--- src/main/java/com/accountmanager/Account.java
+++ src/main/java/com/accountmanager/Account.java
@@ -1,4 +1,35 @@
 package com.accountmanager;
 
+import java.util.ArrayList;
+
+// One account
 public class Account {
+
+    private int accountValue; // Current account value stored in cents
+    private String accountName; // User defined name of account
+    private ArrayList<String> stockSymbols;
+
+    // constructor
+    public Account(int accountValue, String accountName) {
+        this.accountValue = accountValue;
+        this.accountName = accountName;
+    }
+
+    // Set name
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    // Set value
+    public void setAccountValue(int accountValue) {
+        this.accountValue = accountValue;
+    }
+
+    // Get account value
+    public int getAccountValue() {
+        return accountValue;
+    }
+
+    // Add value to account
+    public void addValue(int amount) {}
 }
